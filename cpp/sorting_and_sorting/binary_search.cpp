ff7c9271840c246833245e229e6904bef66247d9
Prosper Awul <awul.prosper@gmail.com>
2024-01-30 16:20:10 +0000

    finished binary search in c++

--- cpp/sorting_and_sorting/binary_search.cpp
+++ cpp/sorting_and_sorting/binary_search.cpp
@@ -9,8 +9,6 @@ int binarySearch(std::vector <int> array, int x){
 
     int begin = 0, end = array.size() - 1;
 
-    
-
     while(begin <= end){
 
         int half = (begin + end)/2;
@@ -18,21 +16,11 @@ int binarySearch(std::vector <int> array, int x){
         if(array[half] == x){
             return array[half];
         }
-
+ 
         if(array[half] > x)
             end = half - 1;
         else 
             begin = half + 1;
     } 
     return - 1;
-}
-
-
-int main(){
-    std::vector<int> ar = {1,2,3,4,5};
-    int x = 2;
-    int ret = binarySearch(ar, x);
-
-    std::cout << ret << "\n";
-    return 0;
 }
